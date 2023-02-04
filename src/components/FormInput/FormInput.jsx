df4240693dfe4c7a79867f78d9f178f4573923a5
yerma4ok <yerma4ok13@gmail.com>
2023-02-04 14:43:09 +0100

    add fix2

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ErrorMessage } from 'formik';
+import styled from 'styled-components';
+import { Field, ErrorMessage } from 'formik';
 import { FormLabel } from './FormInput.styled';
 
-
+const Input = styled(Field)`
+  border: 1px solid lightgray;
+  margin: 20px;
+  margin-top: 5px;
+  font-size: 16px;
+  width: 360px;
+  outline: none;
+  border-radius: 2px;
+  &:hover,
+  &:focus,
+  &:active {
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.2), 0px 1px 1px rgba(0, 0, 0, 0.2),
+      0px 1px 1px rgba(0, 0, 0, 0.2);
+  }
+`;
 
 const FormInput = ({ type, name, label, title }) => {
   return (
--- src/components/FormInput/FormInput.styled.js
+++ src/components/FormInput/FormInput.styled.js
@@ -1,26 +1,9 @@
 import styled from 'styled-components';
-import { Field } from 'formik';
-
 
 export const FormLabel = styled.label`
   font-size: 16px;
   margin-left: 25px;
   margin-top: 20px;
   text-transform: capitalize;
-`;
-
-export const Input = styled(Field)`
-  border: 1px solid lightgray;
-  margin: 20px;
-  margin-top: 5px;
-  font-size: 16px;
-  width: 360px;
-  outline: none;
-  border-radius: 2px;
-  &:hover,
-  &:focus,
-  &:active {
-    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.2), 0px 1px 1px rgba(0, 0, 0, 0.2),
-      0px 1px 1px rgba(0, 0, 0, 0.2);
-  }
+  /* display: block; */
 `;
